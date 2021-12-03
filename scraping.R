4ba448ebdc47e5503c3724380ba76831e2a8b141
lanstand <Lander.Standaert@UGent.be>
2021-12-03 19:32:57 +0100

    update total project

--- scraping.R
+++ scraping.R
@@ -3,38 +3,55 @@ library(rvest)
 library(dplyr)
 library(xml2)
 
-url <- "https://be.indeed.com/jobs?q=%22project+Manager%22&l=Belgi%C3%AB&rbl=Gent&jlid=37bffe97fc19aea0&lang=en"
-page <- xml2::read_html(url)
+page_result_start <- 10 # starting page 
+page_result_end <- 100 # last page results
+page_results <- seq(from = page_result_start, to = page_result_end, by = 10)
 
-job_title = page %>% 
-  html_nodes(".jobTitle") %>% 
-  html_text()
+full_df <- data.frame()
+
+for(i in seq_along(page_results)) {
   
-company_name = page %>% 
-  html_nodes(".companyName") %>% 
-  html_text()
-
-location = page %>% 
-  html_nodes(".companyLocation") %>% 
-  html_text()
-
-links <- page %>% 
-  html_nodes(".tapItem") %>% 
-  html_attr("href")
-
-job_description <- c()
-
-for(i in seq_along(links)) {
-  
-  url <- paste0("https://be.indeed.com", links[i])
+  first_page_url <- "https://be.indeed.com/jobs?q=%22project+Manager%22&l=Belgi%C3%AB&lang=en"
+  url <- paste0(first_page_url, "&start=", page_results[i])
   page <- xml2::read_html(url)
   
-  job_descripition[[i]] = page %>% 
-    html_nodes(".jobsearch-JobComponent-description") %>% 
-    html_text() %>% 
-    stringi::stri_trim_both()
+  # Sys.sleep pauses R for two seconds before it resumes
+  # Putting it there avoids error messages such as "Error in open.connection(con, "rb") : Timeout was reached"
+  Sys.sleep(2)
+ 
+  job_title = page %>% 
+    html_nodes(".jobTitle") %>% 
+    html_text()
+    
+  company_name = page %>% 
+    html_nodes(".companyName") %>% 
+    html_text()
+  
+  location = page %>% 
+    html_nodes(".companyLocation") %>% 
+    html_text()
+  
+  links <- page %>% 
+    html_nodes(".tapItem") %>% 
+    html_attr("href")
+  
+  links
+  job_description <- c()
+  
+  for(i in seq_along(links)) {
+    
+    url <- paste0("https://be.indeed.com", links[i])
+    page <- xml2::read_html(url)
+    
+    job_description[i] = page %>% 
+      html_nodes(".jobsearch-JobComponent-description") %>% 
+      html_text() %>% 
+      stringi::stri_trim_both()
+  }
+  df = data.frame(location, company_name, job_title,  )
+  full_df <- rbind(full_df, df)
 }
 
-df = data.frame(location, company_name, job_title, job_descripition)
+View(full_df)
 
-
+write.csv(full_df,".\\full_df.csv", row.names = FALSE)
--- wordcloud.R
+++ wordcloud.R
@@ -4,12 +4,14 @@ library(wordcloud2)
 library(tm)
 library(dplyr)
 
+#read csv constructed during web scraping
+df = read.csv(file = './full_df.csv')
 
 #Create a vector containing only the text
-text <- job_descripition
+text <- df$job_description
+
 # Create a corpus  
 docs <- Corpus(VectorSource(text))
-
 #cleaning
 docs <- docs %>%
   tm_map(removeNumbers) %>%
@@ -17,7 +19,7 @@ docs <- docs %>%
   tm_map(stripWhitespace)
 docs <- tm_map(docs, content_transformer(tolower))
 docs <- tm_map(docs, removeWords, stopwords("english"))
-docs <- tm_map(docs, removeWords, c("will", "work","can","know","well","make","job","good","things","first","keep","youre")) 
+docs <- tm_map(docs, removeWords, c("will", "work","can","know","well","make","job","good","things","first","keep","youre","ugentec")) 
 
 #basic word stem
 docs <- tm_map(docs, stemDocument)
@@ -28,10 +30,11 @@ matrix <- as.matrix(dtm)
 words <- sort(rowSums(matrix),decreasing=TRUE) 
 df <- data.frame(word = names(words),freq=words)
 
+
 wordcloud2(data=df, size=1.6, color='random-dark')
 
 
 # Find associations for words that occur at least 50 times
-findAssocs(dtm, terms = findFreqTerms(dtm, lowfreq = 50), corlimit = 0.5)
+#findAssocs(dtm, terms = findFreqTerms(dtm, lowfreq = 60), corlimit = 0.6)
 
-df
+filter(df, df$word == "action")
